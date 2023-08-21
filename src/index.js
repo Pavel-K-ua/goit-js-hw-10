de93b0b5fd6a2d61c39e4bb71a2b6c7700f0097e
Pavel-K-ua <pavel.kolosyuk@gmail.com>
2023-08-21 22:03:35 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+import SlimSelect from 'slim-select'
+
+// new SlimSelect({
+//   select: '#single'
+// })
+
 const urlBreeds = `https://api.thecatapi.com/v1/breeds`;
 const api_key = "live_Mfoctkyb81AdTnvkuASKbgDMF3qZlosxU5mxt0kho40Taaq9x32aHUfQTzhcu54D"
-let storedBreeds = []
 // https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
 const refs = {
+    // select: new SlimSelect({
+    //     select: '#selectElement'
+    //   }),
     select: document.querySelector(".breed-select"),
     catInfo: document.querySelector(".cat-info"),
+    loaderEl:document.querySelector(".loader"),
+    errorEl:document.querySelector(".error"),
+    bodyEl: document.body
 }
+
+refs.errorEl.classList.add("is-hidden");
+refs.loaderEl.classList.add("is-hidden");
 
  fetch(urlBreeds,{
     headers: {
@@ -23,15 +37,11 @@ const refs = {
 
 
 function fillSelect (cats) {
-
    const catsArr = cats.map(cat => `
     <option value="${cat.id}">${cat.name}</option>
     `).join("")
     refs.select.insertAdjacentHTML("beforeend", catsArr)
 }
-
-
-// setOutput();
 
 refs.select.addEventListener("change", setOutput);
 
@@ -56,9 +66,25 @@ function setOutput(event) {
  })
  .then((data) => {
     refs.catInfo.innerHTML = ""
-    renderCat(...data)});
-//  зображення і розгорнута інформація про кота: назва породи, опис і темперамент.
+    refs.catInfo.classList.add("is-hidden");
+    refs.loaderEl.classList.remove("is-hidden");
+    // refs.bodyEl.classList.remove("body-bg")
+    
+    renderCat(...data)})
+    .catch((error) => {
+        refs.catInfo.innerHTML = ""
+        refs.errorEl.classList.remove("is-hidden");
+        console.log(error);
+      })
+      .finally(() => {
+        setTimeout(() => {
+        refs.loaderEl.classList.add("is-hidden");
+        refs.catInfo.classList.remove("is-hidden");
+        // refs.bodyEl.classList.add("body-bg")
+        }, 1000);
+      });;
 
+    
 }
 
 function renderCat(cat) {
@@ -70,94 +96,6 @@ function renderCat(cat) {
     <p>${breeds.description}</p>
     <p>${breeds.temperament}</p>`
     refs.catInfo.insertAdjacentHTML("beforeend", markup)
-    console.log(cat);
+    // refs.bodyEl.style.backgroundImage = `url("${cat.url}")`
+    // console.log(cat);
 }
-
-// {
-//     "breeds": [
-//         {
-//             "weight": {
-//                 "imperial": "7 - 15",
-//                 "metric": "3 - 7"
-//             },
-//             "id": "amis",
-//             "name": "Australian Mist",
-//             "temperament": "Lively, Social, Fun-loving, Relaxed, Affectionate",
-//             "origin": "Australia",
-//             "country_codes": "AU",
-//             "country_code": "AU",
-//             "description": "The Australian Mist thrives on human companionship. Tolerant of even the youngest of children, these friendly felines enjoy playing games and being part of the hustle and bustle of a busy household. They make entertaining companions for people of all ages, and are happy to remain indoors between dusk and dawn or to be wholly indoor pets.",
-//             "life_span": "12 - 16",
-//             "indoor": 0,
-//             "lap": 1,
-//             "alt_names": "Spotted Mist",
-//             "adaptability": 5,
-//             "affection_level": 5,
-//             "child_friendly": 4,
-//             "dog_friendly": 5,
-//             "energy_level": 4,
-//             "grooming": 3,
-//             "health_issues": 1,
-//             "intelligence": 4,
-//             "shedding_level": 3,
-//             "social_needs": 4,
-//             "stranger_friendly": 4,
-//             "vocalisation": 3,
-//             "experimental": 0,
-//             "hairless": 0,
-//             "natural": 0,
-//             "rare": 0,
-//             "rex": 0,
-//             "suppressed_tail": 0,
-//             "short_legs": 0,
-//             "wikipedia_url": "https://en.wikipedia.org/wiki/Australian_Mist",
-//             "hypoallergenic": 0,
-//             "reference_image_id": "_6x-3TiCA"
-//         }
-//     ],
-//     "id": "weHGBmpb0",
-//     "url": "https://cdn2.thecatapi.com/images/weHGBmpb0.jpg",
-//     "width": 960,
-//     "height": 854
-// }
-// setOutput();
-
-
-//  <option value="1">Ксенія</option>
-    
-   
-//    //filter to only include those with an `image` object
-//    data = data.filter(img=> img.image?.url!=null)
-   
-//   storedBreeds = data;
-   
-//    for (let i = 0; i < storedBreeds.length; i++) {
-//     const breed = storedBreeds[i];
-//     let option = document.createElement('option');
-     
-//      //skip any breeds that don't have an image
-//      if(!breed.image)continue
-     
-//     //use the current array index
-//     option.value = i;
-//     option.innerHTML = `${breed.name}`;
-// document.getElementById('breed_selector').appendChild(option);
-    
-//     }
-//    //show the first breed by default
-//    showBreedImage(0)
-// })
-// .catch(function(error) {
-//    console.log(error);
-// });
-
-// function showBreedImage(index)
-// { 
-//   document.getElementById("breed_image").src= storedBreeds[index].image.url;
-  
-//   document.getElementById("breed_json").textContent= storedBreeds[index].temperament
-  
-  
-//   document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
-//   document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
-// }
